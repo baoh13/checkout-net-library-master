6627af3de4d7df42808d2eb284e060c4f6108b3d
baoh13 <dzacu1a@gmail.com>
2016-09-05 09:58:25 +0100

    add OrderList

---
+++ Checkout.ApiClient.Net45/ApiServices/Orders/ResponseModels/OrderList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Checkout.ApiServices.Orders.ResponseModels
+{
+    public class OrderList
+    {
+        public int Count;
+        public List<Order> Orders;
+    }
+}
